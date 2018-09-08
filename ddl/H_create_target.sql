d2178c06db9e97b6504d43aeb2914be3e5049d1b
Boopathi <0029@aalamsoft.local>
2018-09-08 17:41:00 +0530

    target script changed

--- ddl/H_create_target.sql
+++ ddl/H_create_target.sql
@@ -1,9 +1,8 @@
 CREATE TABLE `targetFact` (
   `PurchaseID` bigint(20) NOT NULL,
-  `DOP` datetime DEFAULT NULL,
   `CustomerName` varchar(80) NOT NULL,
   `salesAmount` double DEFAULT NULL,
-  `MinAmount` float DEFAULT NULL,
-  `MaxAmount` float DEFAULT NULL,
-  `AvgAmount` double DEFAULT NULL
-);
+  `MinAmount` double DEFAULT NULL,
+  `maxamount` double DEFAULT NULL,
+  `avgamount` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
--- ddl/L_insert_target.sql
+++ ddl/L_insert_target.sql
@@ -1,6 +1,6 @@
-insert into targetFact 
-select sales.ProductID, sales.dop, cust.Name customerName , sum(sales.Amount) salesAmount, min(sales.Amount) MinAmount, max(sales.Amount) MaxAmount, avg(sales.Amount) AvgAmount from db_table_source1 sales
+INSERT INTO targetFact 
+select sales.ProductID,  cust.Name CustomerName , sum(sales.Amount) salesAmount, min(sales.Amount) MinAmount, max(sales.Amount) maxamount, avg(sales.Amount) avgamount from db_table_source1 sales
 inner join db_table_source2 cust on cust.CustomerID = sales.CustomerID
 AND cust.isactive = "Y"
 INNER JOIN db_table_source3 prod on prod.ProductID = sales.ProductID
-group by 1,2,3;
+group by 1,2
