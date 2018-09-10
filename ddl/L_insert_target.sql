7460e8249bc5db4c5e43c1f288aef62d3ae7a2cf
Thiru PS <srikarasu@gmail.com>
2018-09-10 11:42:07 +1000

    Update L_insert_target.sql

--- ddl/L_insert_target.sql
+++ ddl/L_insert_target.sql
@@ -1,6 +1,5 @@
 INSERT INTO targetFact 
 select sales.ProductID,  cust.Name CustomerName , sum(sales.Amount) salesAmount, min(sales.Amount) MinAmount, max(sales.Amount) maxamount, avg(sales.Amount) avgamount from db_table_source1 sales
 inner join db_table_source2 cust on cust.CustomerID = sales.CustomerID
-AND cust.isactive = "Y"
 INNER JOIN db_table_source3 prod on prod.ProductID = sales.ProductID
 group by 1,2
