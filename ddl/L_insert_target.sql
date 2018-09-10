784dfa308a2437382bf1a8155a4394f96e15f47b
Thiru PS <srikarasu@gmail.com>
2018-09-10 12:10:04 +1000

    Update L_insert_target.sql

--- ddl/L_insert_target.sql
+++ ddl/L_insert_target.sql
@@ -1,2 +1,2 @@
 INSERT INTO targetFact 
-select sales.ProductID , cust.Name , sum(sales.Amount) , min(sales.Amount) , max(sales.Amount) , avg(sales.Amount) from cicd.db_table_source1 sales inner join cicd.db_table_source2 cust on cust.CustomerID = sales.CustomerID AND cust.isactive = "Y" INNER JOIN cicd.db_table_source3 prod on prod.ProductID = sales.ProductID group by sales.ProductID,cust.Name
+select sales.ProductID , cust.Name , sum(sales.Amount) , min(sales.Amount) , max(sales.Amount) , avg(sales.Amount) from cicd.db_table_source1 sales inner join cicd.db_table_source2 cust on cust.CustomerID = sales.CustomerID AND cust.isactive = 'Y' INNER JOIN cicd.db_table_source3 prod on prod.ProductID = sales.ProductID group by sales.ProductID,cust.Name
