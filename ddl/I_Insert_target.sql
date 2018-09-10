a21cfe80047bd8b9c9a87bd2ea8a7d80de3bd2c1
Thiru PS <srikarasu@gmail.com>
2018-09-11 00:39:13 +1000

    Update I_Insert_target.sql

--- ddl/I_Insert_target.sql
+++ ddl/I_Insert_target.sql
@@ -1,2 +1,2 @@
 insert into targetFactRel (select cust.company,cust.job_title,sum(ord.shipping_fee) fee from northwind.orders ord 
-inner join northwind.customers cust on cust.id = ord.customer_id where ord.status_id in(1) group by cust.company,cust.job_title);
+inner join northwind.customers cust on cust.id = ord.customer_id where ord.status_id in(0) group by cust.company,cust.job_title);
